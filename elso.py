9d809b61579ede2f1dd300f778af6ed4fa070979
Czotter Gréta <gretaczotter@gmail.com>
2025-09-16 11:02:00 +0200

    kezdet

---
+++ elso.py
@@ -0,0 +1,22 @@
+import random
+
+szamok=[]
+#100 elemű lista feltöltése 1-99 közötti véeletlen számokkal
+
+
+for i in range(100):
+    #...szám generálás
+    rszam=random.randint(1, 100)
+    #szám elhelyezése a listába
+    szamok.append(rszam)
+#ellenőrzés
+print(szamok)
+#egyszám játék
+jatek_szam=0
+nem_talaltdb=0
+
+#kitalálandó szám beállítása
+kitalalando_szam=
+
+
+
